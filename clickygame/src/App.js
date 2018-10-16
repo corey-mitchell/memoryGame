8e6e8964920b78d82f77ecfb1698760d03b2d330
corey-mitchell <corey69_96@yahoo.com>
2018-10-15 23:26:18 -0500

    created react files

---
+++ clickygame/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import components here=====
+
+
+// ===========================
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      {/* Pass Components here */}
+        <h1>Add stuffs here.</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
