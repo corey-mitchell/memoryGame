c7460dedd717289d17e425df1d47ac5f6bf737c2
corey-mitchell <corey69_96@yahoo.com>
2018-10-18 04:12:27 -0500

    added favicon, adjusted some css. should be homework ready!

--- clickygame/src/components/Header.js
+++ clickygame/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
     <header className="header">
-        <h1 className="headerText">Clicky Game!</h1>
+        <h1 className="headerText">Gorillaz Memory Game!</h1>
         <h2 className="headerText">Click on an image to earn points, but don't click on any images more than once!</h2>
     </header>
 );
