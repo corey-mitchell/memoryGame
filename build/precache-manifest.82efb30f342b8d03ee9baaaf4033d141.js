d7c50f332191ab98f7c7462c1aa3c73ace49b022
corey-mitchell <corey69_96@yahoo.com>
2018-10-18 18:48:19 -0500

    add build folder

---
+++ build/precache-manifest.82efb30f342b8d03ee9baaaf4033d141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "007a2667acb7f83acbdbb43b0fdbbfde",
+    "url": "/static/media/background.007a2667.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1689c6a86ba1e01387cf",
+    "url": "/static/js/main.1689c6a8.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "1689c6a86ba1e01387cf",
+    "url": "/static/css/main.eaff2096.chunk.css"
+  },
+  {
+    "revision": "4a311caf6819daeff0a8d2d42a85ea54",
+    "url": "/index.html"
+  }
+];
