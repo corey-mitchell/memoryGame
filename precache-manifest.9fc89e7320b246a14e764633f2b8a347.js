10b25d4e0485ff1f46fa5a706fefc4d77024dca7
corey-mitchell <corey69_96@yahoo.com>
2018-10-18 21:58:40 -0500

    Updates

---
+++ precache-manifest.9fc89e7320b246a14e764633f2b8a347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "007a2667acb7f83acbdbb43b0fdbbfde",
+    "url": "/memoryGame/static/media/background.007a2667.jpg"
+  },
+  {
+    "revision": "f75a57d64a8befc43af4",
+    "url": "/memoryGame/static/js/runtime~main.f75a57d6.js"
+  },
+  {
+    "revision": "01936a0b689348aa6aef",
+    "url": "/memoryGame/static/js/main.01936a0b.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/memoryGame/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "01936a0b689348aa6aef",
+    "url": "/memoryGame/static/css/main.e194a436.chunk.css"
+  },
+  {
+    "revision": "a424d6459f9c3d0e7dfabce7e884edd3",
+    "url": "/memoryGame/index.html"
+  }
+];
